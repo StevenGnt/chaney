730525fc27d41aa3838d8aa7b9ed89a8f13f7111
StevenGnt <stevengangnant@hotmail.com>
2015-03-17 11:07:15 +0100

    Calculator now operating More on input data Gulp now watching data.json

--- app/components/services/calculator.js
+++ app/components/services/calculator.js
@@ -1,66 +1,92 @@
 'use strict';
 angular
-	.module('Chaney')
-	.service('Calculator', function(amMoment) {
-		// Shouldn't this service be a factory ?
-		var calculator = {}, parameters = {},
-			setParameter = function(key, value){
-				parameters[key] = value;
-				return calculator;
-			};
+    .module('Chaney')
+    .service('Calculator', function(amMoment) {
+        // Shouldn't this service be a factory ?
+        var calculator = {}, parameters = {},
+            setParameter = function(key, value){
+                parameters[key] = value;
+                return calculator;
+            },
+            toMoment = function(string) {
+                return amMoment.preprocessors.utc(string, 'DD/MM/YYYY');
+            };
 
-		// Define setters
-		calculator.setStart = function(date){
-			return setParameter('start', date);
-		};
+        // Define setters
+        calculator.setStart = function(date){
+            return setParameter('start', date);
+        };
 
-		calculator.setDuration = function(duration, unit){
-			setParameter('durationUnit', unit || 'month');
-			return setParameter('duration', duration);
-		};
+        calculator.setDuration = function(duration, unit){
+            setParameter('durationUnit', unit || 'month');
+            return setParameter('duration', duration);
+        };
 
-		calculator.setStartValue = function(value) {
-			return setParameter('startValue', value);
-		};
+        calculator.setStartValue = function(value) {
+            return setParameter('startValue', value);
+        };
 
-		calculator.setRecurrings = function(recurrings) {
-			return setParameter('recurrings', recurrings);
+        calculator.setRecurrings = function(recurrings) {
+            return setParameter('recurrings', recurrings);
 
-		};
-		calculator.setUniques = function(uniques) {
-			return setParameter('uniques', uniques);
-		};
+        };
+        calculator.setUniques = function(uniques) {
+            return setParameter('uniques', uniques);
+        };
 
-		calculator.reset = function(){
-			parameters = {};
-			recurrings = [];
-			uniques = [];
-		};
+        calculator.reset = function(){
+            parameters = {};
+            recurrings = [];
+            uniques = [];
+        };
 
-		calculator.computeValues = function() {
-			var values = [],
-				start = amMoment.preprocessors.utc(parameters.start, 'DD/MM/YYYY'),
-				end = start.clone().add(parameters.duration, parameters.durationUnit),
-				count = 0;
+        calculator.computeValues = function() {
+            var values = [],
+                start = toMoment(parameters.start),
+                end = start.clone().add(parameters.duration, parameters.durationUnit);
 
-			values.push(parameters.startValue);
+            // Set initial value
+            values.push(parameters.startValue);
 
-			// Iterate through each day
-			while(start <= end){
-				var value = values[count];
+            // Init values if needed
+            parameters.recurrings = parameters.recurrings || [];
+            parameters.uniques = parameters.uniques || [];
 
-				// @todo:Compute the actual day value according
-				// to Uniques and Recurrings
-				value += Math.random() * 10000;
-				// @todo:end
+            // Iterate through each day
+            while(start <= end){
+                var value = values[values.length - 1],
+                    d = start.format('D'),
+                    string = start.format('DD/MM/YYYY');
 
-				values.push(value)
-				start = start.add(1, 'day');
-				count++;
-			}
+                // Process recurring operations
+                angular.forEach(parameters.recurrings, function(recurring) {
+                    if (d != recurring.day) {
+                        // The value does not apply to the current day
+                        return;
+                    }
 
-			return values;
-		};
+                    if (recurring.start && toMoment(recurring.start) > end
+                     || recurring.end && toMoment(recurring.end) < start) {
+                        // The current recurring is not active
+                        return;
+                    }
 
-		return calculator;
-	});
+                    value += recurring.value;
+                });
+
+                // Process unique operations
+                angular.forEach(parameters.uniques, function(unique) {
+                    if (unique.date === string) {
+                        value += unique.value;
+                    }
+                });
+
+                values.push(value)
+                start = start.add(1, 'day');
+            }
+
+            return values;
+        };
+
+        return calculator;
+    });
