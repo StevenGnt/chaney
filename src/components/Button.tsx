b82dfba6ccbb0063e263a5e03aa030ea2fc84539
Steven Gangnant <stevengangnant@hotmail.com>
2025-12-08 17:22:34 +0100

    Button cursor

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -20,6 +20,7 @@ export function Button({ children, active, onClick }: ButtonProps) {
 				'py-2',
 				'text-sm',
 				'transition',
+				'cursor-pointer',
 				active
 					? ['border-emerald-400/60', 'bg-emerald-400/10', 'text-white']
 					: ['border-white/10', 'bg-black/20', 'text-slate-200', 'hover:border-white/20'],
